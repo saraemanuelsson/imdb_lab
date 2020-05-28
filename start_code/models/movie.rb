c6abc46508c810707761fd306a24564fdde857a0
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-05-28 15:23:15 +0100

    Add initialize methods for stars and movies

--- start_code/models/movie.rb
+++ start_code/models/movie.rb
@@ -1,3 +1,14 @@
 class Movie
 
+    attr_accessor :title, :genre
+    attr_reader :id, 
+
+    def initialize (options)
+        @title = options['title']
+        @genre = options['genre']
+        @id = options['id'].to_i if options['id']
+    end
+
+    
+
 end
--- start_code/models/star.rb
+++ start_code/models/star.rb
@@ -1,3 +1,12 @@
 class Star
 
+    attr_accessor :first_name, :last_name
+    attr_reader :id
+
+    def initialize(options)
+        @first_name = options['first_name']
+        @last_name = options['last_name']
+        @id = options['id'].to_i if options['id']
+    end
+
 end
